0064642462b83da208fe70d434163eef9e7e9369
Josh T <slackerscpt@gmail.com>
2021-12-04 15:04:50 -0700

    Setting up shell for day5

---
+++ Day05/day05.py
@@ -0,0 +1,18 @@
+def readFiles():
+    with open('input.txt', "r") as data:
+    #with open ('test.txt', "r")  as data:
+        inputData = [line.rstrip() for line in data]
+        return inputData
+
+
+
+
+def part1(): 
+    return
+
+def part2():
+    return
+
+
+part1()
+part2()
